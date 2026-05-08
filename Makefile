48f2af7728aec351c71f3afd92843d6ea1382388
Peter Teuben <teuben@gmail.com>
2026-05-08 06:03:14 +0000

    adding the hack to deal with this very peculiar observing session

--- Makefile
+++ Makefile
@@ -6,3 +6,19 @@ include PID
 
 
 # addition PID specific targets can be created here
+
+hack:
+	# step1
+	grep _freq *run1a > hack1a.run
+	grep _freq *run2  > hack1b.run
+	@echo rename 155800_155812  to __CO
+	@echo rename 156134_156163  to __CN
+	# step2
+	grep MWC1_13co *run1a > hack2a.run
+	grep MWC1_13co *run2  > hack2b.run
+	@echo rename 155800_156163 to __13CO
+	# step3
+	grep MWC1_c18o *run1a > hack3a.run
+	grep MWC1_c18o *run2  > hack3b.run
+	@echo rename 155800_156163 to __C18O
+
--- mk_runs.py
+++ mk_runs.py
@@ -1,29 +1,49 @@
-#! /usr/bin/env python
-#
+#! /usr/bin/env python3
 
 import os
 import sys
 
 from lmtoy import runs
 
-project="2023-S1-US-00"
+project="2024-S1-DM-2"
 
-#        obsnums per source (make it negative if not added to the final combination)
+# Dictionary of sources, each with a list of obsnum's in this project
+# negative obsnums are ignored in the combinations. See also comments.txt
+# for obsnum specific comments and parameters!
 on = {}
-on['foo'] = [ ]
 
+on["MWC1_freq1"] = \
+ [ 155800, 155801, 155802, 155804, 155805, 155806, 155810, 155811, 155812,]
 
-#        common parameters per source on the first dryrun (run1a, run2a)
+on["MWC1_freq2"] = \
+ [ 156134, 156138, 156141, 156145, 156159, 156161, 156163,]
+
+on["MWC1_c18o"] = on["MWC1_freq1"] + on["MWC1_freq2"]
+on["MWC1_13co"] = on["MWC1_freq1"] + on["MWC1_freq2"]
+
+# parameters for the first pass of the pipeline (restart=1 is automatically enforced here)
 pars1 = {}
-pars1['foo']   = ""
 
-#        common parameters per source on subsequent runs (run1b, run2b), e.g. bank=0 for WARES
+pars1["MWC1_freq1"] = "vlsr=164 dv=50 dw=50 pix_list=-13,15 bank=1   oid=CO"
+pars1["MWC1_freq2"] = "vlsr=164 dv=50 dw=50 pix_list=-13,15 bank=1   oid=CN"
+pars1["MWC1_c18o"]  = "vlsr=-406 dv=50 dw=50 pix_list=-13 bank=0     oid=C180"
+pars1["MWC1_13co"]  = "vlsr=+736 dv=100 dw=100 pix_list=-13 bank=0   oid=13CO"
+
+# parameters for the (optional) second pass of the pipeline (e.g. for bank=0)
 pars2 = {}
-pars2['foo']   = "srdp=1 admit=0"
 
-#        common parameters per source on subsequent runs (run1c, run2c), e.g. bank=1 for WARES
+pars2["MWC1_freq1"] = "pix_list=-13"
+pars2["MWC1_freq2"] = "pix_list=-13"
+
+# parameters for the (optional) third pass of the pipeline (usually for bank=1)
 pars3 = {}
-pars3['foo']   = "srdp=1 admit=0"
 
-if __name__ == '__main__':    
+pars3["MWC1_freq1"] = ""
+pars3["MWC1_freq2"] = ""
+
+# Found 2 source(s) for 2024-S1-DM-2
+
+if __name__ == "__main__":
     runs.mk_runs(project, on, pars1, pars2, pars3, sys.argv)
+
+# see also /nese/toltec/dataprod_lmtslr/work_lmt/tmp/2024-S1-DM-2.obsnums.log
